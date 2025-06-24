addfd384ec89ff3d14c90ecdef342f15dfaf8a97
Tetrapod 3 Dev <tetrapod3dev@gmail.com>
2025-06-25 00:29:04 +0900

    Docusaurus 구성 파일 수정: 배포 브랜치 'gh-pages' 추가

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -24,6 +24,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'richwon-company',
   projectName: 'richwon-company.github.io',
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
